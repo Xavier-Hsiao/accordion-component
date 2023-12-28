141bbe60b424f6810d9fc01bd6b1f9829e68fdc1
Xavier-Hsiao <tim88776@gmail.com>
2023-12-28 13:47:54 +0800

    Build: render list of AccordionItem component

--- src/component/Accordion.jsx
+++ src/component/Accordion.jsx
@@ -1,5 +1,22 @@
 import styles from "./Accordion.module.scss";
 
-export default function Accordion() {
-  return <div>TODO</div>;
+function AccordionItem({ num, title, text }) {
+  return (
+    <div className={styles.item}>
+      <p className={styles.number}>{num}</p>
+      <p className={styles.text}>{title}</p>
+      <p className={styles.icon}>-</p>
+      <div className={styles.contentBox}>{text}</div>
+    </div>
+  );
+}
+
+export default function Accordion({ data }) {
+  return (
+    <div className={styles.accordion}>
+      {data.map((item, i) => (
+        <AccordionItem title={item.title} text={item.text} num={i} key={i} />
+      ))}
+    </div>
+  );
 }
