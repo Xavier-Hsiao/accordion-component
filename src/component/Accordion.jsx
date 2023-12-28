be014cc7b61a57f098e7c9caccb85b0f181f3fad
Xavier-Hsiao <tim88776@gmail.com>
2023-12-28 14:28:09 +0800

    feat: add AccordionItem toggle feature

--- src/component/Accordion.jsx
+++ src/component/Accordion.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import styles from "./Accordion.module.scss";
 
 function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
   return (
-    <div className={styles.item}>
+    <div
+      className={`${styles.item} ${isOpen ? styles.open : ""}`}
+      onClick={handleToggle}
+    >
       <p className={styles.number}>{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className={styles.title}>{title}</p>
-      <p className={styles.icon}>-</p>
-      <div className={styles.contentBox}>{text}</div>
+      <p className={styles.icon}>{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className={styles.contentBox}>{text}</div>}
     </div>
   );
 }
@@ -15,7 +25,12 @@ export default function Accordion({ data }) {
   return (
     <div className={styles.accordion}>
       {data.map((item, i) => (
-        <AccordionItem title={item.title} text={item.text} num={i} key={i} />
+        <AccordionItem
+          title={item.title}
+          text={item.text}
+          num={i}
+          key={i + item.title}
+        />
       ))}
     </div>
   );
